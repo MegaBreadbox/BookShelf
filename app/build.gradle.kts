fa0df22fb8e4129184f5a0a799ff953f82de3bdd
Joshua <joshuagutierrez724@gmail.com>
2023-09-28 13:07:34 -0400

    Card image works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 }
 
 dependencies {
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
     //retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
 
--- app/src/main/java/com/example/bookshelf/screens/BookshelfApp.kt
+++ app/src/main/java/com/example/bookshelf/screens/BookshelfApp.kt
@@ -1,26 +1,50 @@
 package com.example.bookshelf.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.bookshelf.network.Bookshelf
 
 @Composable
 fun BookShelfApp(
     bookshelfViewModel: BookshelfViewModel = viewModel(),
     modifier: Modifier = Modifier
 ){
-    when(bookshelfViewModel.bookshelfUiState) {
-        is BookUiState.Success -> Text(text = bookshelfViewModel.bookshelfUiState.toString())
+    when(val bookUiState = bookshelfViewModel.bookshelfUiState) {
+        is BookUiState.Success -> BookShelfCard(bookUiState.bookShelf, {})
         is BookUiState.Loading -> Text(text = "Loading")
         is BookUiState.Error -> Text(text = "Error")
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookShelfCard(
-    bookshelfViewModel: BookshelfViewModel,
-    modifier: Modifier = Modifier){
+    bookshelf: Bookshelf,
+    onClick : () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Card(
+        onClick = onClick
+    ){
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(bookshelf.items[0].volumeInfo.imageLinks.thumbnail
+                    .replace("http","https")
+                )
+                .build(),
+            contentDescription = null,
+            modifier = modifier.fillMaxWidth()
+        )
+    }
 
 }
